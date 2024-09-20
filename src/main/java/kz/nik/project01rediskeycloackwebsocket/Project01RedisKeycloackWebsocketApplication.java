ede0acf6e07ef9f92d7184ff8ba0693510882c3e
Nikita Kuzmin <pionerik94@mail.ru>
2024-09-20 20:53:19 +0500

    first commit

---
+++ src/main/java/kz/nik/project01rediskeycloackwebsocket/Project01RedisKeycloackWebsocketApplication.java
@@ -0,0 +1,13 @@
+package kz.nik.project01rediskeycloackwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01RedisKeycloackWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project01RedisKeycloackWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/nik/project01rediskeycloackwebsocket/Project01RedisKeycloackWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nik.project01rediskeycloackwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01RedisKeycloackWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
